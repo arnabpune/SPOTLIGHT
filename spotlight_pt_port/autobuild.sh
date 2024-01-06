496c35c2d49624900fc615af645cb9885c48119b
Venkata Sai (Acer) <venkata.adury0307@gmail.com>
2024-01-06 03:19:24 -0500

    Made linking general

--- spotlight_pt_port/autobuild.sh
+++ spotlight_pt_port/autobuild.sh
@@ -6,16 +6,21 @@ _term() {
 
 trap _term SIGTERM
 
+# What do you want to call your build directory (Default: build). Pass it on as an argument (./autobuild [dirname]) to give a new name
 bd=build
 if test -n "$1"
 then
 	bd=$1
 fi
+
 mkdir $bd
 cd $bd
-export LIBRARY_PATH=$LIBRARY_PATH:"/home/venkata/dnv"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/home/venkata/dnv"
-cmake -DCMAKE_PREFIX_PATH=$HOME/cpp/libtorch ..
-cmake --build . --config Release -- -j 4
+
+loc=`pwd`
+LIBTORCH_LOC="$HOME/cpp/libtorch" #TODO: Change this to match the location you downloaded the libtorch folder. Download from: 
+export LIBRARY_PATH=$LIBRARY_PATH:"$loc/../dnv"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$loc/../dnv"
+cmake -DCMAKE_PREFIX_PATH=$LIBTORCH_LOC ..
+cmake --build . --config Release
 cd ..
 #cmake . --config Release
